f4455f196bb53e874c57a0ebbea963b6678c0b6b
Ahmed Wahied <77901517+AhmedWahied@users.noreply.github.com>
2023-01-01 13:28:50 +0200

    Add files via upload

---
+++ Socket/src/main/java/com/etisalat/socket/Config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.etisalat.socket.Config;
+
+import com.etisalat.socket.handShakeHandler.UserHandshakeHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(final MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/ws");
+    }
+
+    @Override
+    public void registerStompEndpoints(final StompEndpointRegistry registry) {
+        registry.addEndpoint("/myEtisalatSocket")
+                .setHandshakeHandler(new UserHandshakeHandler())
+                .withSockJS();
+    }
+}
